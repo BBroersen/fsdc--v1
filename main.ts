9ea4140967f8c4cb5d63ead747ae8a64084454da
BBroersen <112395027+BBroersen@users.noreply.github.com>
2023-02-13 14:23:16 +0100

    Boot v2

--- main.ts
+++ main.ts
@@ -5,11 +5,15 @@ basic.showLeds(`
     . . . . .
     . . . . .
     `)
+music.playTone(131, music.beat(BeatFraction.Whole))
 basic.showIcon(IconNames.SmallDiamond)
+music.playTone(139, music.beat(BeatFraction.Whole))
 basic.showIcon(IconNames.SmallSquare)
+music.playTone(147, music.beat(BeatFraction.Whole))
 basic.showIcon(IconNames.Square)
 basic.clearScreen()
 basic.pause(200)
+music.playSoundEffect(music.createSoundEffect(WaveShape.Triangle, 942, 2391, 154, 16, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
 basic.showString("On")
 basic.clearScreen()
 basic.pause(200)
