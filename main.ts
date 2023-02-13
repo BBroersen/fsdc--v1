05595f114245839b4cd5ef4b6f6b2310beac10cc
BBroersen <112395027+BBroersen@users.noreply.github.com>
2023-02-13 12:06:40 +0100

    Boot animation

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.showIcon(IconNames.SmallDiamond)
+basic.showIcon(IconNames.SmallSquare)
+basic.showIcon(IconNames.Square)
+basic.clearScreen()
+basic.pause(200)
+basic.showString("On")
+basic.clearScreen()
+basic.pause(200)
 basic.forever(function () {
 	
 })
