f16f4127e5b5de348024526ee48a48b1ee03fee8
BBroersen <112395027+BBroersen@users.noreply.github.com>
2023-02-20 14:02:31 +0100

    Knipperlicht, sirene tijdens rijden/obstacel

--- main.ts
+++ main.ts
@@ -1,24 +1,3 @@
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . # . .
-    . . . . .
-    . . . . .
-    `)
-music.playTone(131, music.beat(BeatFraction.Whole))
-basic.showIcon(IconNames.SmallDiamond)
-music.playTone(139, music.beat(BeatFraction.Whole))
-basic.showIcon(IconNames.SmallSquare)
-music.playTone(147, music.beat(BeatFraction.Whole))
-basic.showIcon(IconNames.Diamond)
-music.playTone(156, music.beat(BeatFraction.Whole))
-basic.showIcon(IconNames.Square)
-basic.clearScreen()
-basic.pause(200)
-music.playSoundEffect(music.createSoundEffect(WaveShape.Triangle, 942, 2391, 154, 16, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
-basic.showString("On")
-basic.clearScreen()
-basic.pause(200)
 basic.forever(function () {
 	
 })
